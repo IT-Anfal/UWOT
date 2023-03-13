d26f5c60c654032a92e2a6ed897af2e436067ffa
IT-Anfal <122197542+IT-Anfal@users.noreply.github.com>
2023-03-13 13:09:04 +0300

    Update 1_About.py

--- pages/1_About.py
+++ pages/1_About.py
@@ -1,20 +1,20 @@
 import streamlit as st
 
-st.set_page_config(page_title="3YON team",
+st.set_page_config(page_title="UWOT team",
                    layout='wide',
                    page_icon='./icons/about.png')
 
-# st.title("3yon Team")
+
 st.subheader("our wonderful team consists of: ")
 st.write("   ●   Maseal Alghamdi, PhD")
-st.write("   ●   Abdullah Alshaya")
-st.write("   ●   Abdullah Alzaben")
+st.write("   ●   Eman Bin Khunayn, PhD")
+st.write("   ●   Abdulaziz Alobaid")
+st.write("   ●   Amal Alsuwaidan")
 st.write("   ●   Anfal AlAwajy")
 st.write("   ●   Nada AlMugren")
-st.write("   ●   Sarah Alghamdi")
+st.write("   ●   Walaa Alsumari")
 
 
-st.write("check out our smartathon [page](https://smartathon.hackerearth.com/challenges/hackathon/smartathon-2/dashboard/ae23dcf/team/?utm_source=team-invite&utm_medium=email&utm_campaign=hackathons)")
 st.write("##")
 st.subheader("Demo of our model: ")
-st.video("https://youtube.com/shorts/vO1KiOklygE")
+# st.video("")
