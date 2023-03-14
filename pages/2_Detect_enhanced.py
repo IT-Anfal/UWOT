a06cb19db5b757c247c28c210b42a4aef5a0dddb
IT-Anfal <122197542+IT-Anfal@users.noreply.github.com>
2023-03-14 10:35:11 +0300

    Add files via upload

---
+++ pages/2_Detect_enhanced.py
@@ -0,0 +1,192 @@
+import streamlit as st
+import torch
+from detect import detect
+from PIL import Image
+from io import *
+import glob
+from datetime import datetime
+import os
+import wget
+import time
+
+## CFG
+cfg_model_path = "models/yourModel.pt" # we cannot upload model to github becuse of large size
+
+cfg_enable_url_download = True #set this to true <-- use this option
+if cfg_enable_url_download:  #Upload model weight and provide url to download it ↓
+    url = "https://archive.org/download/best_20230313/yolov5mclean.pt" #put the model download url
+    cfg_model_path = f"models/{url.split('/')[-1:][0]}" #config model path from url name
+## END OF CFG
+
+# if cfg_enable_url_download:
+#     urls = [ "https://archive.org/download/yoloXSmart/yoloXSmart.pt","https://archive.org/download/yoloLSmart/yoloNSmartPS128.pt", "https://archive.org/download/yoloLSmart/yoloMSmart.pt", "https://archive.org/download/yoloLSmart/yoloSSmartPS128.pt", "https://archive.org/download/yoloLSmart/yoloNSmartPS128.pt"] #Configure this if you set cfg_enable_url_download to True
+#     cfg_model_path = f"models/{urls[0].split('/')[-1:][0]}"
+#     i = 1
+#     for i in range(len(urls)):
+#         cfg_model_path += f" models/{urls[i].split('/')[-1:][0]}" #config model path from url name   
+#          #cfg_model_path = f"models/{url.split('/')[-1:][0]}" #config model path from url name
+# ## END OF CFG
+
+
+
+
+
+
+def imageInput(device, src):
+    
+    if src == 'Upload your own data.':
+        image_file = st.file_uploader("Upload An Image", type=['png', 'jpeg', 'jpg'])
+        col1, col2 = st.columns(2)
+        if image_file is not None:
+            img = Image.open(image_file)
+            with col1:
+                st.image(img, caption='Uploaded Image', use_column_width='always')
+            ts = datetime.timestamp(datetime.now())
+            imgpath = os.path.join('data/uploads', str(ts)+image_file.name)
+            outputpath = os.path.join('data/outputs', os.path.basename(imgpath))
+            with open(imgpath, mode="wb") as f:
+                f.write(image_file.getbuffer())
+
+            #call Model prediction--
+            model = torch.hub.load('ultralytics/yolov5', 'custom', path=cfg_model_path, force_reload=True) 
+            model.cuda() if device == 'cuda' else model.cpu()
+            pred = model(imgpath)
+            pred.render()  # render bbox in image
+            for im in pred.ims:
+                im_base64 = Image.fromarray(im)
+                im_base64.save(outputpath)
+
+            #--Display predicton
+            
+            img_ = Image.open(outputpath)
+            with col2:
+                st.image(img_, caption='Model Prediction(s)', use_column_width='always')
+
+    elif src == 'From test set.': 
+        # Image selector slider
+        imgpath = glob.glob('data/images/enhanced/*')
+        imgsel = st.slider('Select random images from test set.', min_value=1, max_value=len(imgpath), step=1) 
+        image_file = imgpath[imgsel-1]
+        submit = st.button("Detect 🕵🏻!")
+        col1, col2 = st.columns(2)
+        with col1:
+            img = Image.open(image_file)
+            st.image(img, caption='Selected Image', use_column_width='always')
+        with col2:            
+            if image_file is not None and submit:
+                #call Model prediction--
+                model = torch.hub.load('ultralytics/yolov5', 'custom', path=cfg_model_path, force_reload=True) 
+                pred = model(image_file)
+                pred.render()  # render bbox in image
+                for im in pred.ims:
+                    im_base64 = Image.fromarray(im)
+                    im_base64.save(os.path.join('data/outputs', os.path.basename(image_file)))
+                #--Display predicton
+                img_ = Image.open(os.path.join('data/outputs', os.path.basename(image_file)))
+                st.image(img_, caption='Yolo v5 Model M Prediction(s)')
+                #st.image(img_, caption='Model Prediction(s)')
+
+
+
+
+def videoInput(device, src):
+    uploaded_video = st.file_uploader("Upload Video", type=['mp4', 'mpeg', 'mov'])
+    if uploaded_video != None:
+
+        ts = datetime.timestamp(datetime.now())
+        imgpath = os.path.join('data/uploads', str(ts)+uploaded_video.name)
+        outputpath = os.path.join('data/video_output', os.path.basename(imgpath))
+
+        with open(imgpath, mode='wb') as f:
+            f.write(uploaded_video.read())  # save video to disk
+
+        st_video = open(imgpath, 'rb')
+        video_bytes = st_video.read()
+        st.video(video_bytes)
+        st.write("Uploaded Video")
+        detect(weights=cfg_model_path, source=imgpath, device=0) if device == 'cuda' else detect(weights=cfg_model_path, source=imgpath, device='cpu')
+        st_video2 = open(outputpath, 'rb')
+        video_bytes2 = st_video2.read()
+        st.video(video_bytes2)
+        st.write("Model Prediction")
+
+
+def main():
+    # -- Sidebar
+    st.sidebar.title('⚙️Options')
+    datasrc = st.sidebar.radio("Select input source:", ['Upload your own data.','From test set.'])
+    
+        
+                
+    option = st.sidebar.radio("Select input type:", ['Image', 'Video'])
+   
+#     model_choice = st.sidebar.radio("Select Model.", ['yoloXSmart', 'yoloLSmart','yoloMSmart','yoloSSmartPS128','yoloNSmartPS128'])
+    if torch.cuda.is_available():
+        deviceoption = st.sidebar.radio("Select compute Device:", ['cpu', 'cuda'], disabled = False, index=1)
+    else:
+        deviceoption = st.sidebar.radio("Select compute Device:", ['cpu', 'cuda'], disabled = True, index=0)
+    # -- End of Sidebar
+    
+#     img_logo = Image.open(os.path.join('data/images', os.path.basename("smartathonLogo.png")))
+#     st.image(img_logo, caption='')
+    st.header("UWOT")
+    st.header("Underwater Objects Tracking")
+
+#     st.header('👀 Visual Pollution Detection')
+    st.write('👈🏻 Select options left-haned menu bar, then detect')
+    
+    if option == "Image":    
+        imageInput(deviceoption, datasrc)
+    elif option == "Video": 
+        videoInput(deviceoption, datasrc)
+        
+#     if model_choice == 'yoloXSmart':
+#         url = "https://archive.org/download/yoloXSmart/yoloXSmart.pt" #Configure this if you set cfg_enable_url_download to True
+#         cfg_model_path = f"models/{url.split('/')[-1:][0]}" #config model path from url name
+        
+    
+ 
+#     elif model_choice == 'yoloLSmart':
+#         url = "https://archive.org/download/yoloLSmart/yoloLSmart.pt" #Configure this if you set cfg_enable_url_download to True
+#         cfg_model_path = f"models/{url.split('/')[-1:][0]}" #config model path from url name
+        
+        
+#     elif model_choice == 'yoloMSmart':
+#         url = "https://archive.org/download/yoloLSmart/yoloMSmart.pt" #Configure this if you set cfg_enable_url_download to True
+#         cfg_model_path = f"models/{url.split('/')[-1:][0]}" #config model path from url name
+        
+   
+ 
+#     elif model_choice == 'yoloSSmartPS128':
+#         url = "https://archive.org/download/yoloLSmart/yoloSSmartPS128.pt" #Configure this if you set cfg_enable_url_download to True
+#         cfg_model_path = f"models/{url.split('/')[-1:][0]}" #config model path from url name
+        
+ 
+#     else:
+#         url = "https://archive.org/download/yoloLSmart/yoloNSmartPS128.pt" #Configure this if you set cfg_enable_url_download to True
+#         cfg_model_path = f"models/{url.split('/')[-1:][0]}" #config model path from url name
+        
+    
+
+    
+
+if __name__ == '__main__':
+  
+    main()
+
+# Downlaod Model from url.    
+@st.cache
+def loadModel():
+    start_dl = time.time()
+    model_file = wget.download(url, out="models/")
+    finished_dl = time.time()
+    print(f"Model Downloaded, ETA:{finished_dl-start_dl}")
+    
+#     for url in urls:
+#         start_dl = time.time()
+#         model_file = wget.download(url, out="models/")
+#         finished_dl = time.time()
+#         print(f"Model Downloaded, ETA:{finished_dl-start_dl}")
+    
+if cfg_enable_url_download:
+    loadModel()
---
+++ pages/3_About.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(page_title="UWOT team",
+                   layout='wide',
+                   page_icon='./icons/about.png')
+
+
+st.subheader("our wonderful team consists of: ")
+st.write("   ●   Maseal Alghamdi, PhD")
+st.write("   ●   Eman Bin Khunayn, PhD")
+st.write("   ●   Abdulaziz Alobaid")
+st.write("   ●   Amal Alsuwaidan")
+st.write("   ●   Anfal AlAwajy")
+st.write("   ●   Nada AlMugren")
+st.write("   ●   Walaa Alsumari")
+
+
+st.write("##")
+st.subheader("Demo of our model: ")
+# st.video("")
